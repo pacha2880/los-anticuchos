b0e9dddd68e7d4e21697ae3cce49bd2de1c95c0f
pacha2880 <pacha2881@gmail.com>
2026-05-09 22:53:30 -0400

    update basis z2

--- math/basis Z2.cpp
+++ math/basis Z2.cpp
@@ -1,32 +1,13 @@
-const int N=905;
-struct Gaus
-{
-    bitset<N> basis[N];
-    bool val[N];
-    int cmp;
-    bool pos;
-    Gaus(){
-        cmp=0;
-        pos=1;
+int basis[30], si = 0;
+void add(int x) {
+  fore(i, 0, 21)    
+    if(x & 1 << i) {
+      if(basis[i])
+        x ^= basis[i];
+      else {
+        basis[i] = x;
+        si++;
+        return;
+      }
     }
-    void ad(bitset<N> nuevo,bool ult)
-    {
-        for(int i=N-1;i>=0;i--)
-        {
-            if (nuevo[i])
-            {
-                if (basis[i][i])
-                    nuevo^=basis[i],ult^=val[i];
-                else
-                {
-                    basis[i]=nuevo,val[i]=ult;
-                    cmp++;
-                    return;
-                }
-            }
-        }
-        if (ult)
-            pos=0;
-    }
-
-};  
+}
