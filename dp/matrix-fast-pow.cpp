8cb2010ddd620714582e470476f9527b8043afa7
pacha2880 <pacha2881@gmail.com>
2026-07-07 00:16:48 -0400

    update: matrix exp

--- dp/matrix-fast-pow.cpp
+++ dp/matrix-fast-pow.cpp
@@ -1,7 +1,7 @@
 // kth term of linear recurrence 
 // of size m a_i = sum(a_(i-j)*p_j)
 // f(x) = x^m - sum(x^(m-j)*p_j)
-// g(x^k) = g(x^k mod f)
+// g(x^k) = g(x^k MOD f)
 typedef vector<vector<ll> > Matrix;
 Matrix ones(int n) {
   Matrix r(n,vector<ll>(n));
@@ -9,14 +9,14 @@ Matrix ones(int n) {
   return r;
 }
 Matrix operator*(Matrix &a, Matrix &b) {
-  int n=SZ(a),m=SZ(b[0]),z=SZ(a[0]);
+  int n=sz(a),m=sz(b[0]),z=sz(a[0]);
   Matrix r(n,vector<ll>(m));
   fore(i,0,n)fore(j,0,m)fore(k,0,z)
-    r[i][j]+=a[i][k]*b[k][j],r[i][j]%=mod;
+    r[i][j]+=a[i][k]*b[k][j],r[i][j]%=MOD;
   return r;
 }
 Matrix be(Matrix b, ll e) {
-  Matrix r=ones(SZ(b));
+  Matrix r=ones(sz(b));
   while(e){if(e&1LL)r=r*b;b=b*b;e/=2;}
   return r;
 }
